2a5b1cec4bce9d250aa8b590678aece55316ac28
John Louis Del Rosario <john2x@gmail.com>
2015-07-09 10:04:39 +0800

    Bump version to 0.2

--- plan9-theme.el
+++ plan9-theme.el
@@ -4,7 +4,7 @@
 
 ;; Author: John Louis Del Rosario <john2x@gmail.com>
 ;; URL: https://github.com/john2x/plan9-theme.el
-;; Version: 0.1
+;; Version: 0.2
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
